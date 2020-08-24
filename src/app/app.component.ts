9d246a8f5f6633113290fced9ed9c3fd3b160b97
DawidJS <48735427+DawidJS@users.noreply.github.com>
2020-08-24 21:11:35 +0200

    0,1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './models/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ZadanieNr2ToDo';
+
+  public tasks: Task[] = [];
+  public newTask = new Task();
+
+  public addTask(): void {
+    this.newTask.createdDate = new Date();
+    this.newTask.isDone = false;
+    this.tasks.push(this.newTask);
+    this.newTask = new Task();
+  }
+
+  public makeTaskDone(task: Task) {
+    task.isDone = true;
+    task.finishDate = new Date();
+  }
+
+  public makeTaskAvailable(task: Task) {
+    task.isDone = false;
+    task.finishDate = null;
+  }
+
+  public filterTask(isDone) {
+    return this.tasks.filter( x => x.isDone === isDone);
+  }
+
+  ngOnInit(): void {
+    this.newTask = new Task();
+  }
+
+
 }
